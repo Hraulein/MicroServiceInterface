6682247b82290b8b0b2b989907035a1c9db8f5a9
ali <solitude@hraulein.com>
2023-05-06 14:13:01 +0800

    主函数

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ MicroServiceInterface
     1.1
 2. 调用接口api, 获取接口返回信息
     2.1
-"""
 
+"""
+from lib import utils, interface
+from datetime import datetime
 
 if __name__ == '__main__':
-    print('hello')
+    # interface_safety = interface.YamlInterface('interface/safety.yml', 'SAFETY')
+    # print(interface.YamlInterface.interface_safety())
+    # time = datetime.now().strftime('%Y%m%d%H%M%S')
+    # print(time)
+    # print(interface_safety.safety_json('TEST001'))
+    # print(utils.md5_digest('20200819154300EESasdasdA0030016'))
+    # print(interface.YamlInterface.safety_json())
+    params = interface.YamlInterface('./interface/createImagePath.yml', 'PARAMS')
+    print(params.interface_json())
